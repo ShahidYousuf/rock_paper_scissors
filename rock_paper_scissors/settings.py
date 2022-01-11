6ffb4f56cea31d9c25285e454fddfe2bbe4f6771
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-01-11 16:51:57 +0530

    add mysql db backend, add basic gitignore, freeze requirements into requirements text file

--- rock_paper_scissors/settings.py
+++ rock_paper_scissors/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'rock_paper_scissors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rock_paper_scissors_db',
+        'USER': 'USER_HERE',
+        'PASSWORD': 'PASSWORD_HERE'
     }
 }
 
